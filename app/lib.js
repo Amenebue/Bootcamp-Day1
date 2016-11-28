6bcd6aa839c6e8c759852a01ff4774b3d0685470
euniceakerele <eunice.akerele@gmail.com>
2016-11-28 16:16:51 +0100

    Initial commit

---
+++ app/lib.js
@@ -0,0 +1,38 @@
+module.exports={
+	Car: function(name='General', model='GM', vehicle_type=None) {
+    	this.name = name;
+    	this.model = model;
+    	this.vehicle_type = vehicle_type;
+
+    }
+
+}
+    if this.name in ['Porshe', 'Koenigsegg']{
+    	this.num_of_doors = 2
+    }
+    else:
+      	this.num_of_doors = 4
+
+    if 	this.vehicle_type == 'trailer'{
+       	this.num_of_wheels = 8
+    }
+    else:
+      	this.num_of_wheels = 4
+
+
+  function is_saloon(this){
+    if this.vehicle_type is not 'trailer'{
+        this.vehicle_type == 'saloon'
+        return True
+    return False
+    }
+  function drive(this, moving_speed){
+    if moving_speed == 3{
+      Car.speed = 1000
+    }
+    else if moving_speed == 7{
+      Car.speed = 77
+    }
+
+    
+	
---
+++ test/Getprimes.js
@@ -0,0 +1,21 @@
+function getPrime(n) {
+  var prime_number = [ ];
+    for (counter = 2; counter <= n; counter++) {
+    
+    var not_prime = false;
+    var roundnum = Math.floor(counter/2);
+    
+    for (var i =2; i <= roundnum ; i++) {
+      if (counter%i === 0 && i !== counter) {
+        not_prime = true;
+        }
+    }
+    if (not_prime === false) {
+      prime_number.push(counter);
+    }
+  }
+return prime_number;
+}
+
+console.log(getPrime(100))
+
